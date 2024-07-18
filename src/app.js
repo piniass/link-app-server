fe76d427c26bca3983ceee723cfc68697602f856
miguel <miguelpi1998@gmail.com>
2024-07-18 18:42:24 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -7,9 +7,11 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-    origin: ['https://linkeados.vercel.app/', 'http://localhost:5173'],
-    credentials: true
-}))
+    origin: ['https://linkeados.vercel.app', 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(fileUpload({
