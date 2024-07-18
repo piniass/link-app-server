775fff9eca104499454f19b8469fa11b4697fdc8
miguel <miguelpi1998@gmail.com>
2024-07-18 18:45:41 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -12,6 +12,13 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
+
+app.options('*', cors({
+    origin: ['https://linkeados.vercel.app', 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(fileUpload({
