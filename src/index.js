ebec917bff46e42b95da63e528d70d8501ee6ac4
miguel <miguelpi1998@gmail.com>
2024-07-18 14:19:58 +0200

    cambios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { connectDB } from "./db.js";
 
 connectDB();
 
-app.listen(3000, () => {
-    console.log("Server on port 3000");
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log("Corriendo la aplicacion");
 });
+
+export default app;
