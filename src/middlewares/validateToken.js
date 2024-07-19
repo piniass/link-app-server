ad5feb8e65f1ec1f96f4ac62577476a15b22ca90
miguel <miguelpi1998@gmail.com>
2024-07-19 16:54:29 +0200

    clg

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -7,9 +7,13 @@ export const authRequired = (req, res, next) => {
     if(!token) return res.status(401).json({ message: "No hay token, autoización denegada."})
 
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
+        console.log("token verify: ", token)
+        console.log("secret: ", TOKEN_SECRET)
+        console.log("user: ", user)
         if(err) return res.status(403).json({ message: "Token inválido"})
 
         req.user = user
+        console.log("req user: ",req.user)
 
         next()
 
